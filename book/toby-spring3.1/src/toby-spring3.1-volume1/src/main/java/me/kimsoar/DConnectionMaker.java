0bb46759442ffddef207df89257c2b9bec605c82
kimsoar <kimsoar17@gmail.com>
2023-04-08 16:43:26 +0900

    1.3.3 관계설정 책임의 분리

---
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/DConnectionMaker.java
@@ -0,0 +1,17 @@
+package me.kimsoar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "root", "root123"
+        );
+
+        return c;
+    }
+}
--- book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/User.java
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/User.java
@@ -1,2 +1,31 @@
-package me.kimsoar;public class User {
+package me.kimsoar;
+
+public class User {
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

6c5d90d4c01dcff9cd54ed6363d8746a96eb3649
kimsoar <kimsoar17@gmail.com>
2023-04-10 19:16:53 +0900

    1.5.3 용어 정리

--- book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/UserDaoTest.java
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/UserDaoTest.java
@@ -10,8 +10,13 @@ public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        UserDao dao1 = context.getBean("userDao", UserDao.class);
+        UserDao dao2 = context.getBean("userDao", UserDao.class);
 
+
+        System.out.println(dao1);
+        System.out.println(dao2);
+/*
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
@@ -25,6 +30,6 @@ public class UserDaoTest {
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + "조회 성공");
+        System.out.println(user2.getId() + "조회 성공");*/
     }
 }

10f058f534d684bd8a2de5c2e34f35f187388bd2
kimsoar <kimsoar17@gmail.com>
2023-04-11 19:43:27 +0900

    2.3.2 테스트 결과의 일관성

--- book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/springbook/Main.java
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/springbook/Main.java
@@ -1,9 +1,11 @@
 package me.kimsoar.springbook;
 
+import org.junit.runner.JUnitCore;
+
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-
+    public static void main(String[] args) {
+        JUnitCore.main("me.kimsoar.springbook.UserDaoTest");
     }
 }
--- book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/springbook/UserDaoTest.java
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/springbook/UserDaoTest.java
@@ -4,34 +4,39 @@ package me.kimsoar.springbook;
 import me.kimsoar.springbook.dao.DataFactory;
 import me.kimsoar.springbook.dao.UserDao;
 import me.kimsoar.springbook.model.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
-public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-        // ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        // ApplicationContext context = new AnnotationConfigApplicationContext(DataFactory.class);
+
+public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
 
         dao.add(user);
-        
-        System.out.println(user.getId() + "등록 성공");
+        assertThat(dao.getCount(), is(1));
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + "조회 성공");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 }

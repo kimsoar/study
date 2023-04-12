897562eef9258cd85b63bd73034f024b3c2c07a0
kimsoar <kimsoar17@gmail.com>
2023-04-12 19:44:04 +0900

    2.5.2 학습 테스트 예제

---
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/test/java/me/kimsoar/springbook/JUnitTest.java
@@ -0,0 +1,52 @@
+package me.kimsoar.springbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/junit.xml")
+public class JUnitTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    private static Set<JUnitTest> testObjects = new HashSet<>();
+    private static ApplicationContext contextObject = null;
+
+    @Test
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
+    }
+
+    @Test
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
+        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+    }
+
+}

e63dd16ee1a12d4e5ef256c182a7f54cea4d0d16
kimsoar <kimsoar17@gmail.com>
2023-04-16 15:59:16 +0900

    3.6.5 재사용 가능한 콜백의 분리

--- book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/springbook/dao/UserDao.java
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/main/java/me/kimsoar/springbook/dao/UserDao.java
@@ -1,58 +1,38 @@
 package me.kimsoar.springbook.dao;
 
 import me.kimsoar.springbook.model.User;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
-
-    private DataSource dataSource;
-    private JdbcContext jdbcContext;
-
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-        this.jdbcContext = new JdbcContext();
-        this.jdbcContext.setDataSource(dataSource);
-        this.dataSource = dataSource;
     }
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
 
     public void add(final User user) throws SQLException {
         this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?"
-        );
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-        }
-
-        if (user == null) throw new EmptyResultDataAccessException(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return user;
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
     }
 
     public void deleteAll() throws SQLException {
@@ -60,43 +40,10 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
+    }
 
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-
-            return rs.getInt(1);
-
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-
-                } catch (SQLException e) {
-
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-
-                }
-            }
-        }
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
     }
 }
--- book/toby-spring3.1/src/toby-spring3.1-volume1/src/test/java/me/kimsoar/springbook/dao/UserDaoTest.java
+++ book/toby-spring3.1/src/toby-spring3.1-volume1/src/test/java/me/kimsoar/springbook/dao/UserDaoTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -43,9 +44,9 @@ public class UserDaoTest {
         jdbcContext.setDataSource(dataSource);
         this.dao.setDataSource(dataSource);
 
-        this.user1 = new User("gyumee", "박성철", "springno1");
-        this.user2 = new User("leegw700", "이길원", "springno2");
-        this.user3 = new User("park100", "홍길동", "springno3 ");
+        this.user1 = new User("agyumee", "박성철", "springno1");
+        this.user2 = new User("bleegw700", "이길원", "springno2");
+        this.user3 = new User("cpark100", "홍길동", "springno3 ");
     }
 
     @Test
@@ -91,5 +92,37 @@ public class UserDaoTest {
         assertThat(this.dao.getCount(), is(0));
 
         this.dao.get("unkown_id");
+    }
+
+    @Test
+    public void getAll() throws SQLException {
+        dao.deleteAll();
+
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size(), is(0));
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user1, users3.get(0));
+        checkSameUser(user2, users3.get(1));
+        checkSameUser(user3, users3.get(2));
+    }
+
+    private void checkSameUser(User user1, User user2) {
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
     }
 }

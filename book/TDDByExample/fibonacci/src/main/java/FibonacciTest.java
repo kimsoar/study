a731ec815dcf204f94be54f88bb2480f05080161
kimsoar <kimsoar17@gmail.com>
2023-10-08 21:24:34 +0900

    fibonnacci 1 테스트 추가

--- book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
+++ book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
@@ -7,9 +7,12 @@ public class FibonacciTest {
     @Test
     public void testFibonacci() {
         assertEquals(0, fib(0));
+        assertEquals(1, fib(1));
     }
 
     private int fib(int i) {
-        return 0;
+        if (i == 0) return 0;
+
+        return 1;
     }
 }

fbd8c58cbf663ef18556689cb3e339b3b940719e
kimsoar <kimsoar17@gmail.com>
2023-10-08 21:29:01 +0900

    테스트 추가

--- book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
+++ book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
@@ -6,15 +6,16 @@ public class FibonacciTest {
 
     @Test
     public void testFibonacci() {
-        int cases[][] = {{0, 0}, {1, 1}};
+        int cases[][] = {{0, 0}, {1, 1}, {2, 1}, {3,2}};
         for (int i = 0; i < cases.length; i++) {
             assertEquals(cases[i][1], fib(cases[i][0]));
         }
     }
 
-    private int fib(int i) {
-        if (i == 0) return 0;
+    private int fib(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
 
-        return 1;
+        return 2;
     }
 }

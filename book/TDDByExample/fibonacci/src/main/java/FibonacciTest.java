cca4559e3905a174f466ca799dc86315532e37dc
kimsoar <kimsoar17@gmail.com>
2023-10-08 21:31:39 +0900

    fibonnacci 작성 완료

--- book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
+++ book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
@@ -6,7 +6,7 @@ public class FibonacciTest {
 
     @Test
     public void testFibonacci() {
-        int cases[][] = {{0, 0}, {1, 1}, {2, 1}, {3,2}};
+        int cases[][] = {{0, 0}, {1, 1}, {2, 1}, {3, 2}, {4, 3}, {5, 5}, {6, 8}, {7, 13}, {8, 21}};
         for (int i = 0; i < cases.length; i++) {
             assertEquals(cases[i][1], fib(cases[i][0]));
         }
@@ -14,8 +14,8 @@ public class FibonacciTest {
 
     private int fib(int n) {
         if (n == 0) return 0;
-        if (n <= 2) return 1;
+        if (n == 1) return 1;
 
-        return 2;
+        return fib(n - 1) + fib(n - 2);
     }
 }

344eb4d18e799848cafed642df681be89ceff82d
kimsoar <kimsoar17@gmail.com>
2023-10-08 21:23:39 +0900

    fibonnacci 테스트 생성

---
+++ book/TDDByExample/fibonacci/src/main/java/FibonacciTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FibonacciTest {
+
+    @Test
+    public void testFibonacci() {
+        assertEquals(0, fib(0));
+    }
+
+    private int fib(int i) {
+        return 0;
+    }
+}
